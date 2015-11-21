c68bafe359b6d6138a20161382f8e4d3f2c8051a
Piotr Proc <piotr.proc@gmail.com>
2015-11-21 16:45:33 +0100

    Adding Http Server to set up localhost

--- src/main/java/two/game/communication/WebSocketServer.java
+++ src/main/java/two/game/communication/WebSocketServer.java
@@ -1,6 +1,9 @@
 package two.game.communication;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,10 +13,19 @@ public class WebSocketServer extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
+
         vertx.createHttpServer().websocketHandler(ws -> ws.handler(data -> {
             logger.debug("got request {}", data.toString());
             vertx.eventBus().publish("test-address", new JsonObject(data.toString()));
         })).listen(8077);
+
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                String file = req.path().equals("/") ? "index.html" : req.path();
+                req.response().sendFile("./src/main/webapp/" + file);
+            }
+        }).listen(8080);
+
         logger.debug("successfully started handler");
     }
 }
