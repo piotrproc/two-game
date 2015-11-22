969eaf97f437c51f20aa153ed4b1cf67498d9cbc
Michał Żmuda <zmu.michal@gmail.com>
2015-11-22 13:21:27 +0100

    [communication init] incorporated guice into env setup

--- src/main/java/two/game/config/ServerModule.java
+++ src/main/java/two/game/config/ServerModule.java
@@ -3,12 +3,15 @@ package two.game.config;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import io.vertx.core.Verticle;
+import two.game.Server;
 import two.game.communication.WebSocketServer;
 
 public class ServerModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(Server.class);
+
         bind(WebSocketServer.class);
         Multibinder<Verticle> binder = Multibinder.newSetBinder(binder(), Verticle.class);
         binder.addBinding().to(WebSocketServer.class);
