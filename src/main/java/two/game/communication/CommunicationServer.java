e315276162de4bf1495fed9d3f99a6de9fa5e22b
Michał Żmuda <zmu.michal@gmail.com>
2015-11-28 16:13:04 +0100

    [communication init] switched to bridged event bus

--- src/main/java/two/game/communication/CommunicationServer.java
+++ src/main/java/two/game/communication/CommunicationServer.java
@@ -1,7 +1,10 @@
 package two.game.communication;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,19 +13,18 @@ public class CommunicationServer extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().websocketHandler(ws -> ws.handler(data -> {
-            logger.debug("got request {}", data.toString());
-            JsonObject json = new JsonObject(data.toString());
-            handle(json);
-        })).listen(8077);
+        Router router = Router.router(vertx);
+
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        BridgeOptions options = new BridgeOptions();
+        //todo filter messages
+        options.addInboundPermitted(new PermittedOptions().setAddressRegex(".*"));
+        options.addOutboundPermitted(new PermittedOptions().setAddressRegex(".*"));
+        sockJSHandler.bridge(options);
+
+        router.route("/eventbus/*").handler(sockJSHandler);
+        vertx.createHttpServer().requestHandler(router::accept).listen(8077);
 
         logger.debug("successfully started handler");
-    }
-
-    private void handle(JsonObject json) {
-        String address = json.getString("type");
-        JsonObject message = json.getJsonObject("actualMessage");
-        vertx.eventBus().publish(address, message);
-        logger.debug("sent to channel '{}' as {}", address, message);
     }
 }
