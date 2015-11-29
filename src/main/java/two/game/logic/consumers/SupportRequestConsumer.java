5e54e05accea84712abe34ed4b978d39bb64ff4b
althardaine <althardaine@gmail.com>
2015-11-29 18:12:21 +0100

    added consumers initial implementation

--- src/main/java/two/game/logic/consumers/SupportRequestConsumer.java
+++ src/main/java/two/game/logic/consumers/SupportRequestConsumer.java
@@ -1,11 +1,34 @@
 package two.game.logic.consumers;
 
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import two.game.logic.GameState;
+import two.game.logic.predicates.ChangePredicate;
 import two.game.model.update.SupportRequest;
 
+import javax.inject.Named;
+import java.util.Set;
+
 public class SupportRequestConsumer implements EventConsumer<SupportRequest> {
+    private static final Logger logger = LoggerFactory.getLogger(JoinConsumer.class);
+
+    private final Set<ChangePredicate> predicates;
+
+    @Inject
+    public SupportRequestConsumer(@Named("SupportRequest") Set<ChangePredicate> predicates) {
+        this.predicates = predicates;
+    }
+
     @Override
     public void process(SupportRequest event, GameState gameState) {
-        //todo
+        logger.debug("got {}", event);
+        boolean applicable = predicates.stream().allMatch(predicate -> predicate.applicable(event, gameState));
+
+        logger.debug("change is applicable: {}", applicable);
+        if (applicable) {
+            // todo
+            // some notification to the user (with event.getAmount())
+        }
     }
 }
--- src/main/java/two/game/logic/consumers/UnitUpdateConsumer.java
+++ src/main/java/two/game/logic/consumers/UnitUpdateConsumer.java
@@ -1,11 +1,39 @@
 package two.game.logic.consumers;
 
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import two.game.logic.GameState;
+import two.game.logic.predicates.ChangePredicate;
 import two.game.model.update.UnitUpdate;
 
+import javax.inject.Named;
+import java.util.Set;
+
 public class UnitUpdateConsumer implements EventConsumer<UnitUpdate> {
+    private static final Logger logger = LoggerFactory.getLogger(JoinConsumer.class);
+
+    private final Set<ChangePredicate> predicates;
+
+    @Inject
+    public UnitUpdateConsumer(@Named("UnitUpdate") Set<ChangePredicate> predicates) {
+        this.predicates = predicates;
+    }
+
     @Override
     public void process(UnitUpdate event, GameState gameState) {
-        // todo
+        logger.debug("got {}", event);
+        boolean applicable = predicates.stream().allMatch(predicate -> predicate.applicable(event, gameState));
+
+        logger.debug("change is applicable: {}", applicable);
+        if (applicable) {
+            gameState.getUnitStatuses().stream().filter(u -> u.getUnitId() == event.getUnitId()).forEach(u -> {
+                u.setPosition(event.getMoveTarget());
+                // todo
+                // other changes?
+            });
+
+            logger.debug("updated game state (unitUpadate)");
+        }
     }
 }
