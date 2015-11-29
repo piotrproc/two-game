d779649a8a642f46de22e26a47d9f488b3dedf9d
althardaine <althardaine@gmail.com>
2015-11-29 18:42:49 +0100

    added handling of attacks (have to be adjust with type that is not yet available)

--- src/main/java/two/game/logic/consumers/UnitUpdateConsumer.java
+++ src/main/java/two/game/logic/consumers/UnitUpdateConsumer.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import two.game.logic.GameState;
 import two.game.logic.predicates.ChangePredicate;
+import two.game.model.status.UnitStatus;
+import two.game.model.update.UnitAttack;
 import two.game.model.update.UnitUpdate;
 
 import javax.inject.Named;
+import java.util.List;
 import java.util.Set;
 
 public class UnitUpdateConsumer implements EventConsumer<UnitUpdate> {
@@ -27,11 +30,17 @@ public class UnitUpdateConsumer implements EventConsumer<UnitUpdate> {
 
         logger.debug("change is applicable: {}", applicable);
         if (applicable) {
-            gameState.getUnitStatuses().stream().filter(u -> u.getUnitId() == event.getUnitId()).forEach(u -> {
-                u.setPosition(event.getMoveTarget());
-                // todo
-                // other changes?
-            });
+            List<UnitStatus> unitStatuses = gameState.getUnitStatuses();
+            gameState.getUnitStatuses().stream().filter(u -> u.getUnitId().equals(event.getUnitId()))
+                    .forEach(u -> u.setPosition(event.getMoveTarget()));
+
+            for (UnitAttack attack : event.getAttacks()) {
+                UnitStatus attackingUnit = unitStatuses.stream().filter(unit -> unit.getUnitId().equals(event.getUnitId())).findFirst().get();
+                UnitStatus attackedUnit = unitStatuses.stream().filter(unit -> unit.getUnitId().equals(attack.getTargetUnitId())).findFirst().get();
+                //todo
+                //unit has no type so there is no way to determine damage (set temporary to 10)
+                attackedUnit.setHealth(attackedUnit.getHealth() - 10);
+            }
 
             logger.debug("updated game state (unitUpadate)");
         }
